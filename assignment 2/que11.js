930833cbf43cd4e309a61ee1b9de7c1cc2b66d75
symonmuchemi <simonkenya846@gmail.com>
2024-09-13 15:47:20 +0300

    question 11

---
+++ assignment 2/que11.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function to print all the
+// methods in a JavaScript object.
+
+
+const printObjMethods = (obj) => {
+    console.log(Object.getOwnPropertyNames(Array));
+}
+
+
+printObjMethods(Map)
