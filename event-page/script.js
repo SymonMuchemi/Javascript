4c2e56b2dc4023862f465940486088b7263ac040
symonmuchemi <simonkenya846@gmail.com>
2024-10-03 12:29:22 +0300

    update: adds button logic

--- event-page/script.js
+++ event-page/script.js
@@ -1,11 +1,19 @@
-getdata = async () => {
+fetchData = async (sortOption = null) => {
     try {
         const response = await fetch('data.json');
-        const data = await response.json();
+        let data = await response.json();
 
         const container = document.querySelector('.container')
 
         console.log(data);
+
+        if (sortOption === 'price-asc') {
+            data = data.sort((a, b) => a.price - b.price);
+        } else if (sortOption === 'price-desc') {
+            data = data.sort((a, b) => b.price - a.price);
+        } else if (sortOption === 'date') {
+            data = data.sort((a, b) => new Date(a.date) - new Date(b.date));
+        }
 
         let output = '';
 
@@ -20,7 +28,11 @@ getdata = async () => {
                         <p>${date}</p>
                         <p>${company}</p>
                         <p>${price}</p>
-                        <button>Buy Now</button>
+                        <div class='buttons'>
+                            <button>Buy Now</button>
+                            <button>Buy Now</button>
+                            <button>Buy Now</button>
+                        </div>
                     </div>
                 </div>
             `
@@ -32,4 +44,14 @@ getdata = async () => {
     }
 }
 
-getdata();
+
+const sortByPriceAscendingButton = document.getElementById('price-asc');
+const sortByPriceDescendingButton = document.getElementById('price-desc');
+const sortByDateAscendingButton = document.getElementById('date');
+const favouritesButtons = document.getElementById('favourites')
+
+sortByPriceAscendingButton.addEventListener('click', () => fetchData('price-asc'));
+sortByPriceDescendingButton.addEventListener('click', () => fetchData('price-desc'));
+sortByDateAscendingButton.addEventListener('click', () => fetchData('date'));
+
+fetchData();
